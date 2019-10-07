60b8300aaf49f80d1a0058fe478653dd62187dbb
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-07 15:46:16 -0500

    everything is working stretch is complete

--- data/account-router.js
+++ data/account-router.js
@@ -6,12 +6,12 @@ const db = require('./dbConfig');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // get the list of posts from the db
-  // select * from posts
+  // get the list of accounts from the db
+  // select * from accounts
   db.select('*')
     .from('accounts') // all knex commands return a promise
     .then(accounts => {
-      // send the list of posts to the client
+      // send the list of accounts to the client
       res.status(200).json(accounts);
     })
     .catch(error => {
@@ -21,13 +21,13 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // select * from posts where id = req.params.id
+  // select * from accounts where id = req.params.id
   db.select('*')
     .from('accounts')
     .where('id', '=', req.params.id)
     .first()
     .then(account => {
-      // send the post to the client
+      // send the account to the client
       res.status(200).json(account);
     })
     .catch(error => {
@@ -40,8 +40,8 @@ router.post('/', (req, res) => {
   const accountData = req.body;
   // validate the data before saving it to the database. NEVER TRUST THE CLIENT!!
 
-  // insert into posts () values ()
-  // db.insert(postData, 'id').into('posts')
+  // insert into accounts () values ()
+  // db.insert(accountData, 'id').into('accounts')
   db('accounts')
     .insert(accountData, 'id')
     .then(ids => {
